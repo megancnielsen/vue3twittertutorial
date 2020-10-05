ca334f747891fd1a91dd662d581b3b81dd186f3e
mnielsensiliconmtn <69473750+mnielsensiliconmtn@users.noreply.github.com>
2020-10-05 08:43:20 -0600

    commit while working

---
+++ twitter_copycat/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <UserProfile/>
+  </div>
+</template>
+
+<script>
+import UserProfile from "./components/UserProfile";
+
+export default {
+  name: 'App',
+  components: { UserProfile }
+}
+</script>
+
+// scss compiles to css using something like webpack. it allows for nesting classes for organization. 
+// 'scoped' is an attribute that goes in the 'style' tag that applies the classes only to the component that they are written in
+//  leave 'scoped' out of the App.vue
+
+<style lang="scss">
+
+// app is sitting inside the body, which has an auto margin of 8px. Use base.scss
+#app {
+  background-color: whitesmoke;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ twitter_copycat/src/components/TweetItem.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="tweet-item" @click="favoriteTweet(tweet.id)">
+        <div class="user-profile_tweet">
+            <div class="tweet-item_user">
+                @{{ username }}
+            </div>
+            <div class="tweet-item_content">
+                {{ tweet.content }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TweetItem",
+    props: {
+        username: {
+            type: String,
+            required: true
+        },
+        tweet: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        favoriteTweet(id) {
+            this.$emit('favorite', id)
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+.tweet-item {
+    padding: 20px;
+    background-color: whitesmoke;
+    border-radius: 5px;
+    border: 1px solid gray;
+    box-sizing: border-box;
+    cursor: pointer;
+    transition: all 0.25s ease;
+
+    &:hover {
+        transform: scale(1.1, 1.1);
+    }
+
+    .tweet-item_user {
+        font-weight: bold;
+    }
+}
+
+</style>
